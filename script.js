5e0b52caf7698d36cb847a755717151e69d8d865
Elena <elena@localhost.localdomain>
2024-11-10 14:02:45 +0100

    show password again

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ function changeText() {
 if (window.location.pathname.includes("static.html")) {
     setInterval(changeText, 3000);
 }
+
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+}
